d6ddcc34b3d989860a0c01fca91ea3554d344926
love190love <love190love@yahoo.com.tw>
2020-01-03 14:02:13 +0800

    Initial commit

---
+++ 01/src/main/java/com/example/a01/MyClass.java
@@ -0,0 +1,16 @@
+package com.example.a01;
+
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println();
+        for (int i = 65; i < 91; i++) {
+            System.out.print((char) i);
+        }
+        System.out.println();
+        for (int i = 97; i < 123; i++) {
+            System.out.print((char) i);
+        }
+        System.out.println();
+    }
+}
